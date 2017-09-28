625fa9af6d984af3571550fc3947e393c632952f
Arnaud Ambroselli <arnaud.ambroselli@yahoo.fr>
2017-09-28 19:04:23 +0200

    commited the whole compteur site
    with no json yet

---
+++ compteur/js/base.js
@@ -0,0 +1,10 @@
+import Rebase from  're-base'
+
+const base Rebase.createClass ({
+	apiKey: "AIzaSyCjKLn2jhJn9v5u1m9OMAM-1f_IpDny144",
+	authDomain: "compteur-9d776.firebaseapp.com",
+	databaseURL: "https://compteur-9d776.firebaseio.com",
+	projectId: "compteur-9d776",
+});
+
+export default base;
