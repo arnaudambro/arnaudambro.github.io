07ec5b278b385bd625394469b460edafe4ba56c8
Arnaud Ambroselli <arnaud.ambroselli@yahoo.fr>
2017-09-28 20:59:50 +0200

    get JSON counter values
    on arrive à lire le JSON et à pusher les valeurs dans index.html

--- compteur/js/app.js
+++ compteur/js/app.js
@@ -1,32 +1,37 @@
 /*(function () {*/
-import base from '../base';
 
-
-	var counter = document.querySelectorAll('.counter')							//On sélectionne les deux compteurs de la page html, à incrémenter 
+	var counter = document.querySelectorAll('.counter')							//On sélectionne les deux compteurs de la page html, à incrémenter
 
 	for (var i = 0; i < counter.length; i++) {									//On démarre une boucle for pour traiter tous les compteurs
 		(function(j) {															//On enferme l'opération de la boucle for dans une fonction, tu sais pourquoi
 
 			/*----- Démarrage -----*/
-			var countInt = parseInt(counter[j].textContent) 					//Soit le compteur existe déjà, et on l'appelle
-			if (isNaN(countInt)) {												//Soit c'est la première fois qu'on charge la page, et on l'initialise à 0
-				countInt = 0
-				counter[j].textContent = countInt
-			}			
+      var countInt = parseInt(counter[j].textContent)           //Soit le compteur existe déjà, et on l'appelle
+      if (isNaN(countInt)) {                        //Soit c'est la première fois qu'on charge la page, et on l'initialise à 0
+        countInt = 0
+      }
+      var httpRequest1 = new XMLHttpRequest()              //On crée un objet pour interagir avec les serveurs
+      httpRequest1.open('GET', 'https://arnaudambro.github.io/compteur/js/compteur.json') //On initialise une requête pour les serveurs
+      httpRequest1.send()
+      httpRequest1.onreadystatechange = function () {
+        if (httpRequest1.readyState === XMLHttpRequest.DONE /*DONE === 4*/ && httpRequest1.status === 200) {
+          var results1 = JSON.parse(httpRequest1.responseText)
+          countInt = results1[j].counter
+          //countInt++
+          console.log(countInt)
+          counter[j].textContent = countInt
+        }
+      }
 
 			/*----- Incrémentation du compteur -----*/
 			counter[j].addEventListener('click', function (e) {
 				e.preventDefault()												//Faudra qu'on m'explique pourquoi, j'ai pas trop compris encore
-/*
-				var httpRequest = new XMLHttpRequest()							//On crée un objet pour interagir avec les serveurs
-				console.log('httpRequest.status: ' + httpRequest.status + ', httpRequest.readyState: ' +  httpRequest.readyState)  //Pour comprendre
 
-				httpRequest.open('GET', 'https://github.com/arnaudambro/compteur/blob/master/js/compteur.json') //On initialise une requête pour les serveurs
-				console.log('httpRequest.status: ' + httpRequest.status + ', httpRequest.readyState: ' +  httpRequest.readyState)  //Pour comprendre
+				var httpRequest = new XMLHttpRequest()							//On crée un objet pour interagir avec les serveurs
+
+				httpRequest.open('GET', 'https://arnaudambro.github.io/compteur/js/compteur.json') //On initialise une requête pour les serveurs
 
 				httpRequest.send()
-				console.log('httpRequest.status: ' + httpRequest.status + ', httpRequest.readyState: ' +  httpRequest.readyState)  //Pour comprendre
-*/
 				httpRequest.onreadystatechange = function () {
 					if (httpRequest.readyState === XMLHttpRequest.DONE /*DONE === 4*/ && httpRequest.status === 200) {
 						var results = JSON.parse(httpRequest.responseText)
